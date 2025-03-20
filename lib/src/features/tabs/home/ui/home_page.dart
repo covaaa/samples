39edf17a8fcae095b97bacbbe046d4b35f52da2c
cova <cova8458@gmail.com>
2025-03-20 14:11:05 +0900

    feat: update HomePage layout to use CustomScrollView with SliverAppBar.large

--- lib/src/features/tabs/home/ui/home_page.dart
+++ lib/src/features/tabs/home/ui/home_page.dart
@@ -16,15 +16,23 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Samples')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text('$_counter', style: context.headlineMedium),
-          ],
-        ),
+      body: CustomScrollView(
+        slivers: [
+          const SliverAppBar.large(title: Text('Samples')),
+          SliverToBoxAdapter(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // * _LargeScrollUnderFlexibleConfig at material/app_bar.dart
+                // * expandedHeight = 152.0;
+                // * expandedTitlePadding.bottom = 28.0;
+                const SizedBox(height: 152 + 28),
+                const Text('You have pushed the button this many times:'),
+                Text('$_counter', style: context.headlineMedium),
+              ],
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
