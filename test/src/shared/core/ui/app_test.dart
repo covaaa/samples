fb6c05e76c8d02dd48346ea7740a0310a8f11d2c
cova <cova8458@gmail.com>
2025-03-21 01:53:28 +0900

    test: update widget test descriptions for clarity and accuracy

--- test/src/shared/core/ui/app_test.dart
+++ test/src/shared/core/ui/app_test.dart
@@ -1,10 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:samples/src/features/tabs/home/home.dart';
 import 'package:samples/src/shared/core/core.dart';
 import '../../../../extensions/pump.dart';
 
 void main() {
-  testWidgets('App should render HomePage', (tester) async {
+  testWidgets('app should render home page', (tester) async {
     await tester.pumpMaterialApp(scaffold: false, child: const App());
-    expect(find.byType(App), findsOneWidget);
+    expect(find.byType(HomePage), findsOneWidget);
   });
 }
